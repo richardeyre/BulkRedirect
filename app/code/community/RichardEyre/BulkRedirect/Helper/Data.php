368886f9fa41ad8fdb3684009ca979a32ad12267
Richard <hello@richardeyre.me.uk>
2013-01-22 21:47:04 +0000

    Set up the initial module files and configuration

---
+++ app/code/community/RichardEyre/BulkRedirect/Helper/Data.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Richard Eyre Bulk Redirect
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * @category    RichardEyre
+ * @package     RichardEyre_BulkRedirect
+ * @copyright   Copyright (c) 2013 Richard Eyre (http://richardeyre.me.uk)
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+class RichardEyre_Bulkredirect_Helper_Data extends Mage_Core_Helper_Abstract {
+    
+}
---
+++ app/code/community/RichardEyre/BulkRedirect/controllers/Adminhtml/BulkRedirectController.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Richard Eyre Bulk Redirect
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * @category    RichardEyre
+ * @package     RichardEyre_BulkRedirect
+ * @copyright   Copyright (c) 2013 Richard Eyre (http://richardeyre.me.uk)
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+class RichardEyre_BulkRedirect_Adminhtml_BulkRedirectController extends Mage_Adminhtml_Controller_Action {
+ 
+    /**
+     * Initialize the action
+     * @return \RichardEyre_BulkRedirect_Adminhtml_BulkRedirectController
+     */
+    protected function _initAction()
+    {
+        $this->loadLayout()
+            ->_setActiveMenu('catalog')
+        ;
+        return $this;
+    }
+
+    /**
+     * Import URL rewrites from a CSV file
+     */
+    public function importAction()
+    {
+
+        $this->_initAction();
+        $this->renderLayout();
+
+    }
+
+    /**
+     * Export URL rewrites to a CSV file
+     */
+    public function exportAction()
+    {
+        $this->_initAction();
+        $this->renderLayout();
+
+    }
+
+}
